c1332445528417a379fa2839f861f38434b73d3f
Ronny Rook <info@ronnyrook.nl>
2024-01-18 10:21:36 +0100

    Visual representation to reorder blocks

--- src/components/modules/konva/modals/ReorderModal.tsx
+++ src/components/modules/konva/modals/ReorderModal.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 import { type ModalProps } from 'hooks';
+import { useBlocks } from 'store/board';
+import { cn } from 'utils';
 import { Button } from 'common/interaction/Button';
 import {
   Dialog,
@@ -8,6 +12,8 @@ import {
   DialogTrigger,
 } from 'common/interaction/Dialog';
 
+import { useKonvaContext } from '../KonvaContext';
+
 export function ReorderModal({
   children,
   disabled,
@@ -16,6 +22,22 @@ export function ReorderModal({
   onOpen,
   isOpen,
 }: ReorderModalProps) {
+  const { selected } = useKonvaContext()!;
+  const blocks = useBlocks();
+
+  const [switchBlock, setSwitchBlock] = useState<number | null>(null);
+  const selectedOrder = selected !== null ? blocks[selected].order : null;
+
+  function onSetSwitchBlock(order: number) {
+    if (selectedOrder === order) return;
+    setSwitchBlock(order);
+  }
+
+  function onReorderBlocks() {
+    if (!switchBlock || !selectedOrder) return;
+    // @TODO change block.order of both block, sort on order in store
+  }
+
   return (
     <Dialog open={isOpen}>
       <DialogTrigger
@@ -35,9 +57,29 @@ export function ReorderModal({
         </DialogHeader>
 
         <div className="pt-4 pb-8 px-8">
+          <div className="flex">
+            {blocks.map((block) => {
+              return (
+                <button
+                  onClick={() => onSetSwitchBlock(block.order)}
+                  className={cn(
+                    'w-16 h-16 flex items-center justify-center text-2xl border-2 border-primary text-primary bg-transparent shadow-md cursor-pointer mr-4 last:mr-0',
+                    {
+                      'bg-primary text-white':
+                        selectedOrder === block.order || switchBlock === block.order,
+                    },
+                  )}
+                >
+                  {block.order}
+                </button>
+              );
+            })}
+          </div>
+
           <Button
             onClick={onCallback}
             className="mt-8"
+            disabled={!switchBlock || !selectedOrder}
             size="xl"
           >
             Reorder
